b3fa54e15bb90daeba5f3a8abeaa7cc59177e683
anhergi <55754125+anhergi@users.noreply.github.com>
2020-10-10 21:09:04 +0200

    Revert "[project/first-app] Subida Ficheros"

--- first-app/src/containers/BotonesCrud.js
+++
@@ -1,8 +0,0 @@
-import Botones2 from './Botones2'
-import { connect } from 'react-redux'
-
-const mapStateToProps = state => ({
-    paises: state.paises
-})
-
-export default connect(mapStateToProps)(Botones2)
